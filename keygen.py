5a72ed7e5f01d1859368b63e8ac885b85269cac4
pkka.armor@gmail.com <pkka.armor@gmail.com>
2022-07-05 17:52:25 +1000

    Test commit

--- keygen.py
+++ keygen.py
@@ -1,7 +1,19 @@
 
+def ask_for_a_password():
+    pass
+
+def get_password_len():
+    password_len = 0
+    while password_len >= 0:
+        password_len = int(input("Введите длину необходимого пароля: "))
+    return password_len
+
+
 def main():
-    previous_password = input("Для генерации нового пароля будет использована \
-часть старого, введите старый пароль: ")
+    password_len = get_password_len()
+    print(password_len)
+#     previous_password = input("Для генерации нового пароля будет использована \
+# часть старого, введите старый пароль: ")
 
 if __name__ == "__main__":
     main()
