3ca0305aabaf1c7fb213cc2e90f58a075b3859b8
pkka.armor@gmail.com <pkka.armor@gmail.com>
2022-07-06 15:15:47 +1000

    Some refactoring and testing done

--- keygen.py
+++ keygen.py
@@ -55,6 +55,22 @@ def check_for_requirements(alphabet, password):
             password_check = True
     return password_check
 
+def generate_password_base(password_len, words, alphabet):
+    generated_password_len = 0
+    words_to_include = random.choices(words, k=2)
+    for word in words_to_include:
+        generated_password_len += len(word)
+            
+    for _ in range(password_len - generated_password_len - 2):
+        symbol = generate_symbols(alphabet)
+        symbol_position = random.randint(1, 3)  # 1 - before 1st, 2 - between 1 and 2, 3 - after 2nd
+        if symbol_position == 1: words_to_include[0] = symbol + words_to_include[0]
+        elif symbol_position == 2: words_to_include[0] += symbol
+        else: words_to_include[1] += symbol
+
+    return words_to_include
+
+
 def generate_password(password_len, domen):
     words = upload_words()
     alphabet = upload_alphabet()
@@ -63,33 +79,36 @@ def generate_password(password_len, domen):
         password_check = False
         while not password_check:
             password = ""
-            generated_password_len = 0
             if domen == 1: password += "A#"
             elif domen == 2: password += "S#"
 
-            words_to_include = random.choices(words, k=2)
-            for word in words_to_include:
-                generated_password_len += len(word)
-            
-            for _ in range(password_len - generated_password_len - 2):
-                symbol = generate_symbols(alphabet)
-                symbol_position = random.randint(1, 3)  # 1 - before 1st, 2 - between 1 and 2, 3 - after 2nd
-                if symbol_position == 1: words_to_include[0] = symbol + words_to_include[0]
-                elif symbol_position == 2: words_to_include[0] += symbol
-                else: words_to_include[1] += symbol
+            password_base = generate_password_base(password_len, words, alphabet)
+            password += ''.join(password_base)
 
-            password += ''.join(words_to_include)
-
-            password_check = check_for_requirements(alphabet, password)
-
+            password_check = check_for_requirements(alphabet, password[2:])
         generated_passwords.append(password)
     return generated_passwords    
         
+def choose_password(passwords):
+    print("\nПредлагаем вам сгенерированные пароли, выберите один из них:")
+    for i, password in enumerate(passwords):
+        print(f"{i + 1}. {password}")
+    user_choice = -1
+    while user_choice < 0:
+        user_choice = input("\nВведите номер понравившегося варианта: ")
+        if user_choice.isdecimal() and 1 <= int(user_choice) <= len(passwords):
+            print(f"\nВы выбрали {int(user_choice)}! Ваш пароль: {passwords[int(user_choice) - 1]}")
+            return passwords[int(user_choice) - 1]
+        else:
+            print("Введите правильный номер!")
+            user_choice = -1
+
 # S# is for sigma password, A# is for alpha
 def main():
-    password_len = 20 #get_password_len()
+    password_len = 16 #get_password_len()
     domen = get_password_domen(password_len)
-    print(generate_password(password_len, domen))
+    list_to_choose = generate_password(password_len, domen)
+    print(choose_password(list_to_choose))
     
 if __name__ == "__main__":
     main()
